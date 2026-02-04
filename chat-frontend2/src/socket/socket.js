508f15699ca05424c6e4796706cb043d862633e8
Chandnibai04 <chandnirathi04@gmail.com>
2026-02-04 16:07:52 +0500

    set the file of socket

--- chat-frontend2/src/socket/socket.js
+++ chat-frontend2/src/socket/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "https://localhost:5000";
-export const socket = io(BASE_URL, {
-  transports: ["websocket"], // force WSS
-  withCredentials: true,
-});
 
+const SOCKET_URL = process.env.REACT_APP_BACKEND_URL;
+
+export const socket = io(SOCKET_URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
